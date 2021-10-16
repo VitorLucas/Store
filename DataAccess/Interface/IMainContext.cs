01a94de632296cd9c26cf1e116a48fcb079ddc96
Vitor L Santos <vitor@spectralsolutions.com.br>
2021-10-16 10:23:51 -0300

    Creation and implementation of MainContext interface

---
+++ DataAccess/Interface/IMainContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interface
+{
+    public interface IMainContext
+    {
+        DbSet<Category> Category { get; set; }
+        DbSet<Product> Product { get; set; }
+        DbSet<User> User { get; set; }
+        DbSet<Role> Role { get; set; }
+
+        int SaveChanges();
+    }
+}
--- DataAccess/MainContext.cs
+++ DataAccess/MainContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccess.Interface;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Model;
 
 namespace DataAccess
 {
-    public partial class MainContext : DbContext
+    public partial class MainContext : DbContext, IMainContext
     {
         public DbSet<Category> Category { get; set; }
         public DbSet<Product> Product { get; set; }
@@ -12,10 +13,11 @@ namespace DataAccess
         public DbSet<Role> Role { get; set; }
 
         public MainContext(DbContextOptions<MainContext> options) : base(options) { }
-       
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -24,6 +26,11 @@ namespace DataAccess
             UserMap(modelBuilder);
             ProductMap(modelBuilder);
             ProductMap(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
         }
     }
 }
